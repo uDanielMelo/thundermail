8440e5d5f27b888f6ce6e257b097a57b0440a50e
uDanielMelo <daniel.melo002@gmail.com>
2026-03-19 20:09:44 -0300

    feat: app accounts com login, cadastro e dashboard

---
+++ apps/accounts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = 'accounts'
+
+urlpatterns = [
+    path('cadastro/', views.cadastro, name='cadastro'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+]
