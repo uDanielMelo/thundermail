8154b391dc49c0840069b58784a0ee198b9131b1
uDanielMelo <daniel.melo002@gmail.com>
2026-03-19 21:42:23 -0300

    feat: app analytics com logs e métricas de campanha

--- core/urls.py
+++ core/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('accounts/', include('apps.accounts.urls', namespace='accounts')),
     path('contacts/', include('apps.contacts.urls', namespace='contacts')),
     path('campaigns/', include('apps.campaigns.urls', namespace='campaigns')),
+    path('analytics/', include('apps.analytics.urls', namespace='analytics')),
     path('dashboard/', accounts_views.dashboard, name='dashboard'),
 ]
