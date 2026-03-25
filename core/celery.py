1a4c1518328fe5d199af26c34d5c662c6581811f
uDanielMelo <daniel.melo002@gmail.com>
2026-03-25 20:27:45 -0300

    fix: carrega .env automaticamente no Celery

--- core/celery.py
+++ core/celery.py
@@ -1,8 +1,16 @@
 import os
 from celery import Celery
+from dotenv import load_dotenv
+
+load_dotenv()
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-
 app = Celery('core')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
+app.conf.beat_schedule = {
+    'verificar-campanhas-agendadas': {
+        'task': 'apps.campaigns.tasks.send_scheduled_campaigns',
+        'schedule': 60.0,
+    },
+}
